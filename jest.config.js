3b80df41ca768b91edd2f12a8b649b4763c90d3c
jordy248 <jordy@jordynelson.io>
2023-12-01 21:54:02 -0500

    update jest config for 2023 :clown:

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 /** @type {import('jest').Config} */
 const config = {
   verbose: true,
-  testPathIgnorePatterns: ['/node_modules/', '/2021/'],
+  testPathIgnorePatterns: ['/node_modules/', '/2021/', '/2022/'],
   setupFilesAfterEnv: ['./utils/setupTests.js'],
   transform: {
     '\\.js$': ['babel-jest', { configFile: './babel-jest.config.cjs' }],
