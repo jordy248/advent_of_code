66a8081f8f2bb70356f07d1bcd24a41c4547cf3a
jordy248 <jordy@jordynelson.io>
2024-12-03 23:30:46 -0500

    update utils :triangular_ruler:

--- src/utils/Array.js
+++ src/utils/Array.js
@@ -5,3 +5,25 @@ export function sumArray(a) {
 export function countOccurrences(arr, val) {
   return arr.filter((v) => v === val).length;
 }
+
+export function getIsMonotonic(arr, type='either') {
+  const allowedTypes = ['increase', 'decrease', 'either'];
+  if (!allowedTypes.includes(type.toLowerCase())) return;
+
+  if (arr.some((val) => typeof val !== 'number')) {
+    throw new Error('array must consist of numbers');
+  }
+
+  if (type.toLowerCase() === 'increase') {
+    const isMonotonicallyIncreasing = arr.every((val, i) => i === 0 || val > arr[i - 1]);
+    return isMonotonicallyIncreasing;
+  } else if (type.toLowerCase() === 'decrease') {
+    const isMonotonicallyDecreasing = arr.every((val, i) => i === 0 || val < arr[i - 1]);
+    return isMonotonicallyDecreasing;
+  } else {
+    const isMonotonicallyIncreasing = arr.every((val, i) => i === 0 || val > arr[i - 1]);
+    const isMonotonicallyDecreasing = arr.every((val, i) => i === 0 || val < arr[i - 1]);
+    const isMonotonicallyChanging = isMonotonicallyIncreasing || isMonotonicallyDecreasing;
+    return isMonotonicallyChanging;
+  }
+}
